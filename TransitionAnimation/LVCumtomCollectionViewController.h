f6975d9ff886d5c936dda597c35cc65e6778b2df
yingwei0831 <birneysky@163.com>
2016-09-19 20:54:42 +0800

    modify

---
+++ TransitionAnimation/LVCumtomCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  LVCumtomCollectionViewController.h
+//  TransitionAnimation
+//
+//  Created by birneysky on 16/9/19.
+//  Copyright © 2016年 com.github. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LVCumtomCollectionViewController : UIViewController
+
+@end
---
+++ TransitionAnimation/LVCumtomCollectionViewController.m
@@ -0,0 +1,37 @@
+//
+//  LVCumtomCollectionViewController.m
+//  TransitionAnimation
+//
+//  Created by birneysky on 16/9/19.
+//  Copyright © 2016年 com.github. All rights reserved.
+//
+
+#import "LVCumtomCollectionViewController.h"
+
+@interface LVCumtomCollectionViewController ()
+
+@end
+
+@implementation LVCumtomCollectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
